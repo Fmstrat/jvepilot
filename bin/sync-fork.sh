d1350ad5f63c36be493f9e89d85b7f76e5111b96
Fmstrat <nospam@nowsci.com>
2023-01-17 10:15:50 -0500

    updated sync-fork for beta

--- bin/sync-fork.sh
+++ bin/sync-fork.sh
@@ -5,6 +5,8 @@ UPSTREAM="https://github.com/j-vanetten/openpilot.git"
 set -e
 if [ -z "$(git remote -v |grep upstream)" ]; then
     set -x
+    git checkout --track origin/jvePilot-beta
+    git checkout jvePilot-release
     git remote add upstream ${UPSTREAM}
     { set +x; } 2>/dev/null
 fi
@@ -18,4 +20,6 @@ git checkout jvePilot-beta
 git pull
 git merge upstream/jvePilot-beta
 git push
+git checkout jvePilot-release
 { set +x; } 2>/dev/null
+
